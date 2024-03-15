afa4eb418efea77d764fbb033495f6397264e197
pvk <sergpilk33@gmail.com>
2024-03-15 16:42:41 +0300

    v1

---
+++ PRACT_LAB_1_EF/Posts_page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRACT_LAB_1_EF
+{
+    /// <summary>
+    /// Логика взаимодействия для Posts_page.xaml
+    /// </summary>
+    public partial class Posts_page : Page
+    {
+        private VodocanalEntities VOD = new VodocanalEntities();
+        public Posts_page()
+        {
+            InitializeComponent();
+            Table.ItemsSource = VOD.Posts.ToList();
+        }
+    }
+}
